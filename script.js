7185494b1a65941da9a90f67bf19616307527063
meshackc0de <meshackjohn420@gmail.com>
2025-08-04 00:16:58 +0300

    add script.js

--- script.js
+++ script.js
@@ -10,7 +10,34 @@ document.addEventListener('DOMContentLoaded',()=> {
   const username = document.getElementById('username').value.trim();
   const email = document.getElementById('email').value.trim();
   const password = document.getElementById('password').value.trim();
-  
 
-  
+  let isValid = true;
+  let messages = [];
+
+  if(username.length < 3){
+    isValid === false;
+    messages.push('Short Password');
+  }
+
+  if(email.includes('@') && email.includes('.')){
+    isValid === true;
+  } else {
+    isValid === false;
+    messages.push('Include @ or .');
+  }
+
+  if(password.length < 8){
+    isValid === false;
+    messages.push('Password is not long enough')
+  }
+
+  feedbackDiv.style.display = 'block';
+  if(isValid === true){
+    feedbackDiv.textContent = 'Registration successful!';
+    feedbackDiv.style.color = '#28a745';
+  } else {
+    feedbackDiv.innerHTML = messages.join('<br>');
+    feedbackDiv.style.color = '#dc3545';
+
+  }
 });
