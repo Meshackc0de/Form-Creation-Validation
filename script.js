9d6bd04e938acc305c9ced4d9712306322d31e13
meshackc0de <meshackjohn420@gmail.com>
2025-08-04 09:39:30 +0300

    script.js update

--- script.js
+++ script.js
@@ -1,43 +1,46 @@
 document.addEventListener('DOMContentLoaded',()=> {
-  const form = document.getElementById('#registration-form');
-  const feedbackDiv = document.getElementById('#form-feedback');
+  const form = document.getElementById('registration-form');
+  const feedbackDiv = document.getElementById('form-feedback');
 
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-  } )
-
-  const username = document.getElementById('username').value.trim();
-  const email = document.getElementById('email').value.trim();
-  const password = document.getElementById('password').value.trim();
-
-  let isValid = true;
-  let messages = [];
-
-  if(username.length < 3){
-    isValid === false;
-    messages.push('Short Password');
-  }
-
-  if(email.includes('@') && email.includes('.')){
-    isValid === true;
-  } else {
-    isValid === false;
-    messages.push('Include @ or .');
-  }
-
-  if(password.length < 8){
-    isValid === false;
-    messages.push('Password is not long enough')
-  }
-
-  feedbackDiv.style.display = 'block';
-  if(isValid === true){
-    feedbackDiv.textContent = 'Registration successful!';
-    feedbackDiv.style.color = '#28a745';
-  } else {
-    feedbackDiv.innerHTML = messages.join('<br>');
-    feedbackDiv.style.color = '#dc3545';
-
-  }
+    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+  
+    let isValid = true;
+    let messages = [];
+  
+  
+  //username validation
+    if(username.length < 3){
+      isValid = false;
+      messages.push('Username must be at least 3 characters long.');
+    }
+  //email validation
+    if(!email.includes('@') || !email.includes('.')){
+      isValid = false;
+      messages.push("Email must include '@' and '.'");
+    }
+    
+    //password validation
+  
+    if(password.length < 8){
+      isValid = false;
+      messages.push('Password must be at least 8 characters long')
+    }
+  
+    //display feedback
+    feedbackDiv.style.display = 'block';
+    if(isValid){
+      feedbackDiv.textContent = 'Registration successful!';
+      feedbackDiv.style.color = '#28a745';
+    } else {
+      feedbackDiv.innerHTML = messages.join('<br>');
+      feedbackDiv.style.color = '#dc3545';
+    }
+  });
+  
 });
+
